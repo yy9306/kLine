8723d0f3616f2195c963ca2f8c4259db078a60fc
军哥哥 <1273460656@qq.com>
2020-12-27 18:35:27 +0800

    更新

--- stack-module/src/component/pageComponent.tsx
+++ stack-module/src/component/pageComponent.tsx
@@ -2,27 +2,61 @@
 import stack from './stack'
 class Component{
   public stackContainer: any
+  public root: HTMLElement
   constructor() {
     this.stackContainer = stack();
+    this.root = document.getElementById('root') as HTMLElement;
   }
   mounted() {
     console.log('这里不实现具体的方法');
   }
   addNode(root: HTMLElement) {
     const nodeStr:string = this.render();
-    let childLists = root.childNodes;
+    let childLists: NodeListOf<HTMLElement> = this.root.childNodes as NodeListOf<HTMLElement>;
+    const len = childLists.length;
+    const currentNode: HTMLElement = childLists[len - 1] as HTMLElement;
+    // const nextNode: HTMLElement = childLists[len - 2] as HTMLElement;
     if (childLists.length <= 0) {
       root.innerHTML = nodeStr as string 
     } else {
-      (childLists[childLists.length - 1] as HTMLElement).style.display = "none";
-      (document.querySelector('.main') as HTMLElement).insertAdjacentHTML('afterend', nodeStr);
+      // (childLists[childLists.length - 1] as HTMLElement).style.display = "none";
+      this.addClass(currentNode, 'slide-leave-left');
+      currentNode.insertAdjacentHTML('afterend', nodeStr);
+      currentNode.addEventListener('animationend', () => {
+        // currentNode.style.display = 'none';
+      })
     }
     this.stackContainer.push(nodeStr);
     this.mounted()
     this.handleMethods(nodeStr);
+    
   }
   removeNode() {
-    this.stackContainer.pop()
+    let childLists: NodeListOf<HTMLElement> = this.root.childNodes as NodeListOf<HTMLElement>;
+    const len = childLists.length;
+    const currentNode: HTMLElement = childLists[len - 1] as HTMLElement;
+    const nextNode: HTMLElement = childLists[len - 2] as HTMLElement;
+    this.removeClass(currentNode, nextNode);
+    this.addClass(currentNode, 'slide-leave-right');
+    this.addClass(nextNode, 'slide-enter-left');
+    nextNode.style.display = 'block';
+    setTimeout(() => {
+      childLists[len - 1].remove();
+      this.stackContainer.pop();
+      console.log(this.stackContainer)
+    }, 400)
+    // nextNode.addEventListener('animationend', _that.remove)
+  }
+  removeClass(...nodes: any) {  
+    nodes.forEach((item: any) => {
+      const classes: string = (item as HTMLElement).getAttribute('class') as string;
+      if (classes.includes('slide')) {
+        (item as HTMLElement).setAttribute('class', classes.replace(/(\s*)slide-.+?(\s|$)/g, ''));
+      }
+    });
+  }
+  addClass(node: HTMLElement, className: string) {
+    (node as HTMLElement).classList.add(className)
   }
   handleMethods(nodeStr: string) {
     const _that = this;
@@ -31,7 +65,6 @@ class Component{
     methods = nodeStr.match(reg) as string[];
     methods && methods.forEach(item => {
       item.replace(/.+\{(\w+).+\"([\w\_\-]+)\".+/g, function(...args: string[]) {
-        console.log(args[1]);
         (document.querySelector(`.${args[2]}`)) &&  (document.querySelector(`.${args[2]}`) as HTMLElement).addEventListener("click", () => _that[args[1]]())
         return ''
       })
