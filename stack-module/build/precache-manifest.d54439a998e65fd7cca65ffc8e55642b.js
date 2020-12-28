1806818fcfaac30f4e84c29dc3b11174cc453069
军哥哥 <1273460656@qq.com>
2020-12-28 14:11:07 +0800

    build

---
+++ stack-module/build/precache-manifest.d54439a998e65fd7cca65ffc8e55642b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1249ceaa7921f5e47148c1ad0423e6a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff35b3eb370746ad4822",
+    "url": "/static/css/main.23038b28.chunk.css"
+  },
+  {
+    "revision": "ff35b3eb370746ad4822",
+    "url": "/static/js/main.cc4c2d4c.chunk.js"
+  },
+  {
+    "revision": "59aeca5868cac9ee1852",
+    "url": "/static/js/runtime-main.8937e534.js"
+  },
+  {
+    "revision": "f484ab9059d37557323d107f24aa7ab2",
+    "url": "/static/media/avatar.f484ab90.jpg"
+  }
+]);
